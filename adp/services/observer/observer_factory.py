f8fdf4d7e0795fd562956d29057f62050d4e074d
qxnam <quachnam311@gmail.com>
2026-01-27 22:56:38 +0700

    chores: refactor save markdown

--- adp/services/observer/observer_factory.py
+++ adp/services/observer/observer_factory.py
@@ -1,12 +1,16 @@
-from adp.services.observer.targets.api import HttpTarget
+from adp.services.observer.targets.local_target import LocalTarget
+from adp.services.observer.targets.s3_target import S3Target
 
+class ObserverFactory:
+    _mapping = {
+        "local": LocalTarget,
+        "s3": S3Target
+    }
 
-def create_observer() -> None:
-    """
-    Factory function to create observer instances based on configuration.
-
-    Returns:
-        An instance of a subclass of `BaseObserver` corresponding to the specified type in configuration.
-    """
-    # code here
-    pass
+    @classmethod
+    def create_observer(cls, target_name: str):
+        target_class = cls._mapping.get(target_name.lower())
+        if not target_class:
+            raise ValueError(f"Target '{target_name}' is not supported.")
+        return target_class()
+    
